06fb892ea6e9fcc278a73c708379aa0f253615a6
Jean-Yves CHAILLOU <jeanyves.chaillou@gmail.com>
2022-08-07 23:39:24 +0200

    filter on rules list

--- src/app/components/rules-list/rules-list.component.ts
+++ src/app/components/rules-list/rules-list.component.ts
@@ -9,9 +9,9 @@ import { applyDrag } from '../../utils/utils';
 export class RulesListComponent implements OnInit {
   @Input() id:number=0;
   @Input() rules:any;
+  ruleFilter:string = '';
   @Input() scope:any;
   @Input() redirectTypes:any;
-
   @Output()
   onEmitChangeSaveRule = new EventEmitter<any>();
   @Output()
@@ -29,5 +29,16 @@ export class RulesListComponent implements OnInit {
   onDrop(dropResult:any) {
     console.log(dropResult)
     this.rules = applyDrag(this.rules, dropResult);
+  }
+  onChangeSearchEvent(event:any){
+    console.log('onChangeSearchEvent', event)
+    this.ruleFilter = event.target.value;
+  }
+  get filteredRules():any{
+    if(this.ruleFilter == '') return this?.rules;
+    let rg = new RegExp(this.ruleFilter)
+    return this.rules.filter((r:any) =>{
+      return rg.test(r.origin) || rg.test(r.target);
+    });
   }
 }
