74cea720fb4d1f06bc061ab91b17920762ed032c
kwabounga <jeanyves.chaillou@gmail.com>
2022-08-29 15:47:57 +0200

    work on deleting scope

--- src/app/pages/delete-scope/delete-scope.component.ts
+++ src/app/pages/delete-scope/delete-scope.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { DataFromIpcService } from 'src/app/services/data-from-ipc.service';
 
 @Component({
   selector: 'app-delete-scope',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-scope.component.css']
 })
 export class DeleteScopeComponent implements OnInit {
+  @ViewChild('deleteModal') modalRef!:any;
+  scopeSelected:boolean = false;
+  scopeSelectedName:string = '';
+  scopeSelectedId:number = -1;
+  constructor(
+    private dataSrv:DataFromIpcService
+  ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  scopes:any;
+  async ngOnInit(): Promise<void> {
+    this.scopes = await this.dataSrv.getScopesAll();  
   }
-
+  change(event:any){
+    const id = event.target.value;
+    console.log(id);
+    if(id){
+      this.scopeSelectedId = id;
+      this.scopeSelectedName = event.target.options[id].text;
+      console.log(this.scopeSelectedName);
+      this.scopeSelected = true;
+    } else {
+      this.scopeSelected = false;
+    }
+  }
+  deleteScopeConfig(){
+    console.log('DELETE SCOPE HERE')
+  }
 }
