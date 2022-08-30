5aef78bacae5f1575478cfcf195d1019903a034d
kwabounga <jeanyves.chaillou@gmail.com>
2022-08-30 06:50:31 +0200

    command line for create scope config

---
+++ src/commands/cmd-create-scope-config.js
@@ -0,0 +1,22 @@
+
+console.log(process.argv);
+const scopeID = +process.argv[2]
+if(!scopeID){
+  process.exit(1);
+}
+
+const dbAccess = require("../electron/dbAccess");
+
+console.log(scopeID);
+dbAccess.insertScopesConfig({
+  scope_id: scopeID,
+  label: "",
+  config: `
+RewriteCond %{HTTP_HOST} ^placeHolder$ [NC]
+RewriteRule ^(.*)$ https://www.placeHolder.com/$1 [R=301,L]
+  `,
+  condition: "www.placeHolder.com",
+}).then((result)=>{
+  console.log(scopeID,'config created',result);
+  process.exit(0);
+})
