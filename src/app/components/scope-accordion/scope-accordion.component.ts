cc69cb0d8c3f0effd952e8f5c738816fa0c649c5
kwabounga <jeanyves.chaillou@gmail.com>
2022-09-14 06:02:38 +0200

    accordion n toggle bugfix > angular-cli optim

--- src/app/components/scope-accordion/scope-accordion.component.ts
+++ src/app/components/scope-accordion/scope-accordion.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output,EventEmitter,AfterViewInit,ElementRef, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ContainerComponent, DraggableComponent } from 'ngx-smooth-dnd';
 import { applyDrag } from '../../utils/utils';
+import { Button } from 'bootstrap';
 
 @Component({
   selector: 'app-scope-accordion',
   templateUrl: './scope-accordion.component.html',
   styleUrls: ['./scope-accordion.component.css']
 })
-export class ScopeAccordionComponent implements OnInit {
+export class ScopeAccordionComponent implements OnInit,AfterViewInit {
   @Input() scopes:any;
   @Input() scopeConfig:any = {
     label: "",
@@ -24,11 +25,29 @@ export class ScopeAccordionComponent implements OnInit {
   @Output() onRulesPositions = new EventEmitter();
   @Output() onScope = new EventEmitter();
   @Output() onScopesPosition = new EventEmitter();
-
+  btnAccordion?: Button;
   isOpen = false;
-  constructor(protected sanitizer: DomSanitizer) { }
+  @ViewChild('btnAcc') btn!:ElementRef;
+  constructor(protected sanitizer: DomSanitizer, private elem: ElementRef) { }
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit(){
+    /* let btnTriggers = this.elem.nativeElement.querySelectorAll('.accordion-button')
+    console.log(btnTriggers)
+
+    this.btnAccordion = btnTriggers.call().map((btnTrigger:any)=>{
+      console.log(btnTrigger)
+      return new Button(btnTrigger);
+    }) */
+
+  }
+  clickBtn(event:any){
+    this.isOpen=!this.isOpen
+    console.log(event.path[0])
+    /* this.btnAccordion = new Button(event.path[0])
+    console.log(this.btnAccordion)
+    this.btnAccordion.toggle() */
   }
   saveConfig(event:any){
     this.onConfig.emit(event)
